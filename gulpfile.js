5e5ac3bb22a8c7ac710131fe5f7a7cb301ed2b7c
Denys Izhetskui <Denys Izhetskui>
2015-12-17 15:01:58 +0200

    restructure + gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,65 @@
+// plugins
+
 var gulp          = require('gulp');
-var sass          = require('gulp-sass');
+
 var browserSync   = require('browser-sync').create();
 var reload        = browserSync.reload({stream: true});
+
+var sass          = require('gulp-sass');
+
+var postcss       = require('gulp-postcss');
+var autoprefixer  = require('autoprefixer');
+var mqpacker      = require('css-mqpacker');
+
+var wiredep       = require('wiredep').stream;
+
+var useref        = require('gulp-useref');
+
+var gulpif        = require('gulp-if');
+var uglify        = require('gulp-uglify');
+var minifyCss     = require('gulp-minify-css');
+
+
+// config
+
+var config = {
+  server: {},
+  devPath: {},
+  buildPath: {}
+}
+
+
+// build
+
+gulp.task('css:build', function(){
+
+  var processors = [
+    autoprefixer({browsers: ['last 2 versions'], cascade: false}),
+    mqpacker()
+  ];
+
+  gulp.src('./static/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compact'}).on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./static/css/'));
+
+})
+
+gulp.task('html:build', function () {
+    return gulp.src('./html/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('./build/html/'));
+});
+
+gulp.task('build', [
+    'html:build',
+    'css:build'
+]);
+
+
+// dev
 
 gulp.task('sass', function(){
 
@@ -12,7 +70,19 @@ gulp.task('sass', function(){
 
 });
 
-gulp.task('serve', ['sass'], function(){
+gulp.task('html', function () {
+  gulp.src('./html/*.html')
+    .pipe(wiredep())
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('./html-preview/'))
+    .pipe(reload({stream: true}));
+});
+
+
+gulp.task('serve', ['html', 'sass'], function(){
   
   browserSync.init({
     notify: false,
@@ -20,10 +90,11 @@ gulp.task('serve', ['sass'], function(){
       baseDir: './' ,
       directory: true
     },
+    startPath: './html-preview/'
   });
 
   gulp.watch('./html/*.html').on('change', function(){
-    browserSync.reload();
+    gulp.run('html');
   });
 
   gulp.watch('./static/scss/**/*.scss' , ['sass']);
